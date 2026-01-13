9a45ec1cae15c102f6910daa1988c1a14b4d9cba
zoltanmurai <zoltanmurai@gmail.com>
2026-01-13 11:23:25 +0100

    debug sync

--- ProfitProphet/obj/Debug/net9.0-windows/ProfitProphet.AssemblyInfo.cs
+++ ProfitProphet/obj/Debug/net9.0-windows/ProfitProphet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProfitProphet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e3fef9c5bc27837a7271d49a7233bcbbc1041f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03480558053943a7bb8124632a052be44509a08a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProfitProphet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProfitProphet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
